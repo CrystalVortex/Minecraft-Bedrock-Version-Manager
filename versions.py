a01c23bce4fe87a2a7c51396192ae3b6705317e9
nobody1256 <99749845+nobody1256@users.noreply.github.com>
2022-04-24 15:55:22 +0200

    Update versions.py

--- versions.py
+++ versions.py
@@ -4,7 +4,7 @@ import webbrowser
 
 root = Tk()
 
-root.geometry('1024x1024')  
+root.geometry('512x512')  
 
 
 new = 1
@@ -16,6 +16,16 @@ url2 = "https://download2388.mediafire.com/n4u8hpavbxag/j3gn0szp5zriw6z/Microsof
 new = 3
 url3 = "https://download2391.mediafire.com/9oijpuvi4xag/zq0pcoqbsfbhvfw/Minecraft-1.18.12.1.Appx"
 
+new = 4 
+url4 = "https://download2340.mediafire.com/mgy9tfbk8yvg/5o9wz0ltbb6xv5x/Minecraft-1.18.2.3.Appx"
+
+new = 5
+url5 = "https://download2267.mediafire.com/n1susudxioqg/kmelqlxa658388m/Minecraft-1.18.1.2.Appx"
+
+new = 6 
+url6 = "https://download2390.mediafire.com/wa607c69vbug/wjekuyje36e51ql/Minecraft-1.18.0.2.Appx"
+
+
 def openweb():
     webbrowser.open(url1,new=new)
 
@@ -24,6 +34,17 @@ def openweb2():
 
 def openweb3():
     webbrowser.open(url3,new=new)
+
+def openweb4():
+    webbrowser.open(url4,new=new)
+
+def openweb5():
+    webbrowser.open(url5,new=new)
+
+def openweb6():
+    webbrowser.open(url6,new=new)
+
+
 
 Btn = Button(root, text = "Download 1.18.30 64 bit",command=openweb)
 Btn.pack()
@@ -34,4 +55,13 @@ Btn.pack()
 Btn = Button(root, text = "Download 1.18.10 64 bit",command=openweb2)
 Btn.pack()
 
+Btn = Button(root,text = "Download 1.18.2.3 64 bit",command=openweb4)
+Btn.pack()
+
+Btn = Button(root,text = "Download 1.18.1 64 bit",command=openweb5)
+Btn.pack()
+
+Btn = Button(root,text= "Download 1.18 64 bit",command=openweb6)
+
 root.mainloop()
+
