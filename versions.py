7b63c74b65c75fe5934a248a8a00e4132ab44e30
nobody1256 <99749845+nobody1256@users.noreply.github.com>
2022-04-24 15:22:02 +0200

    Update versions.py

--- versions.py
+++ versions.py
@@ -13,7 +13,8 @@ url1 = "https://download2344.mediafire.com/ec85gea86keg/p5yxrsv39uvgdo0/Microsof
 new = 2
 url2 = "https://download2388.mediafire.com/n4u8hpavbxag/j3gn0szp5zriw6z/Microsoft.MinecraftUWP_1.18.1004.0_x86__8wekyb3d8bbwe.Appx"
 
-
+new = 3
+url3 = "https://download2391.mediafire.com/9oijpuvi4xag/zq0pcoqbsfbhvfw/Minecraft-1.18.12.1.Appx"
 
 def openweb():
     webbrowser.open(url1,new=new)
@@ -21,10 +22,16 @@ def openweb():
 def openweb2():
     webbrowser.open(url2,new=new)
 
-Btn = Button(root, text = "Download 1.18.30 (x86)",command=openweb)
+def openweb3():
+    webbrowser.open(url3,new=new)
+
+Btn = Button(root, text = "Download 1.18.30 64 bit",command=openweb)
 Btn.pack()
 
-Btn = Button(root, text = "Download 1.18.10 (x86)",command=openweb2)
+Btn = Button(root, text = "Download 1.18.12 64 bit",command=openweb3)
+Btn.pack()
+
+Btn = Button(root, text = "Download 1.18.10 64 bit",command=openweb2)
 Btn.pack()
 
 root.mainloop()
