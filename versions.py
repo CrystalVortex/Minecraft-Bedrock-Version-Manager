8413878b999f7fa7be6217e813936d39e5148532
nobody1256 <99749845+nobody1256@users.noreply.github.com>
2022-04-28 15:24:10 +0200

    Update versions.py

--- versions.py
+++ versions.py
@@ -10,11 +10,27 @@ import datetime
 
 import time
 
+import logging
+
+logger = logging.getLogger('Vlauncher')
+
+handler = logging.FileHandler('VLauncher.log')
+
+logger.addHandler(handler)
+
 print("Starting Application...")
+
+logger.warning('Logging started')
+
+logger.warning('[LOG]:Starting Application...')
 
 time.sleep(3)
 
 print("[LOG]:Application started")
+
+logger.warning('[LOG]:Application started')
+
+logger.warning('[LOG]:Window Name:Minecraft Version Launcher (512x512)')
 
 ws = Tk()
 
@@ -25,16 +41,16 @@ ws.geometry('512x512')
 
 
 new = 1
-url1 = "https://download2344.mediafire.com/ec85gea86keg/p5yxrsv39uvgdo0/Microsoft.MinecraftUWP_1.18.3004.0_x86__8wekyb3d8bbwe.Appx"
+url1 = "https://download2344.mediafire.com/7lo7d4se8m0g/p5yxrsv39uvgdo0/Microsoft.MinecraftUWP_1.18.3004.0_x86__8wekyb3d8bbwe.Appx"
 
 new = 2
-url2 = "https://download2388.mediafire.com/n4u8hpavbxag/j3gn0szp5zriw6z/Microsoft.MinecraftUWP_1.18.1004.0_x86__8wekyb3d8bbwe.Appx"
+url2 = "https://download2265.mediafire.com/13ngbxye53ng/j3gn0szp5zriw6z/Microsoft.MinecraftUWP_1.18.1004.0_x86__8wekyb3d8bbwe.Appx"
 
 new = 3
-url3 = "https://download2391.mediafire.com/9oijpuvi4xag/zq0pcoqbsfbhvfw/Minecraft-1.18.12.1.Appx"
+url3 = "https://download2391.mediafire.com/vembjkpt9kug/zq0pcoqbsfbhvfw/Minecraft-1.18.12.1.Appx"
 
 new = 4 
-url4 = "https://download2340.mediafire.com/mgy9tfbk8yvg/5o9wz0ltbb6xv5x/Minecraft-1.18.2.3.Appx"
+url4 = "https://download2340.mediafire.com/8vts0fml23gg/5o9wz0ltbb6xv5x/Minecraft-1.18.2.3.Appx"
 
 new = 5
 url5 = "https://download2267.mediafire.com/n1susudxioqg/kmelqlxa658388m/Minecraft-1.18.1.2.Appx"
@@ -48,31 +64,38 @@ def openweb():
     webbrowser.open(url1,new=new)
     print("[LOG]:You clicked download 1.18.30 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18.30')
+
 
 def openweb2():
     webbrowser.open(url2,new=new)
     print("[LOG]:You clicked download 1.18.10 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18.10')
 
 def openweb3():
     webbrowser.open(url3,new=new)
     print("[LOG]:You clicked download 1.18.12.1 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18.12.1')
 
 def openweb4():
     webbrowser.open(url4,new=new)
     print("[LOG]:You clicked download 1.18.2.3 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18.2.3')
 
 def openweb5():
     webbrowser.open(url5,new=new)
     print("[LOG]:You clicked download 1.18.1.2 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18.1.2')
 
 def openweb6():
     webbrowser.open(url6,new=new)
     print("[LOG]:You clicked download 1.18 at:")
     print(datetime.datetime.now())
+    logger.warning('[LOG]:You clicked download 1.18')
 
 
 
@@ -100,12 +123,16 @@ ws.mainloop()
 
 print("[LOG]:Application closed!")
 
+logger.warning('[LOG]:Application closed!')
+
 time.sleep(1)
 
 print("[LOG]:Press enter to continue")
 
+logger.warning('[LOG]:Press enter to continue')
+
 input()
 
-
+logger.warning('[LOG]:Application closed...')
 
 
