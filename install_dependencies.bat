9bfd634d52128b68cf845af662a5b36c62a53646
nobody1256 <99749845+nobody1256@users.noreply.github.com>
2022-06-14 21:03:47 +0200

    Delete install_dependencies.bat

--- install_dependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title MBVM: Installing dependencies...
-pip3 install customtkinter
-pip3 install requests
-pip4 install termcolor
-title MBVM: Complete installation...
-echo complete
-
-PAUSE
